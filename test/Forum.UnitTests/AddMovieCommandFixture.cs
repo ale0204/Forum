192a95db7a53cdf4374807877a0cd8e0f202fbbb
ale0204 <nalexandra291@gmail.com>
2025-05-20 20:17:37 +0300

    Adds testing

---
+++ test/Forum.UnitTests/AddMovieCommandFixture.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using Bogus;
+using Forum.Application.Core.Movies.Commands.AddMovie;
+
+namespace Forum.UnitTests;
+
+public class AddMovieCommandFixture
+{
+    private readonly Fixture _fixture;
+    private readonly Faker _faker;
+    public AddMovieCommandFixture()
+    {
+        _fixture = new Fixture();
+        _faker = new Faker();
+    }
+
+    public AddMovieCommand Create(
+        string? title = null,
+        string? description = null,
+        int? duration = null,
+        float? score = null,
+        string? posterUrl = null,
+        DateOnly? launchDate = null,
+        bool useTitle = true,
+        bool useDescription = true,
+        bool useDuration = true,
+        bool useScore = true,
+        bool usePosterUrl = true,
+        bool useLaunchDate = true
+        )
+    {
+        return new Faker<AddMovieCommand>()
+            .CustomInstantiator(f => new AddMovieCommand(
+                Title: useTitle ? (title ?? f.Name.FullName()) : null,
+                Description: useDescription ? (description ?? f.Company.CompanyName()) : null,
+                Duration: useDuration ? (duration ?? f.Random.Int(90, 210)) : null,
+                Score: useScore ? (score ?? f.Random.Float(1, 5)) : null,
+                PosterUrl: usePosterUrl ? (posterUrl ?? f.Internet.Url()) : null,
+                LaunchDate: useLaunchDate ? (launchDate ?? f.Date.BetweenDateOnly(new DateOnly(1950, 1, 1), new DateOnly(2025, 5, 1))) : null
+            ))
+            .Generate();
+    }
+    
+    public List<AddMovieCommand> CreateMany(int count = 3)
+    {
+        return Enumerable.Range(0, count)
+            .Select(_ => Create())
+            .ToList();
+    }
+}
---
+++ test/Forum.UnitTests/AddMovieCommandHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using Forum.Application.Common.DataAccess.Entities;
+using Forum.Application.Common.DataAccess.Repositories;
+using Forum.Application.Common.DataAccess.UoW;
+using Forum.Application.Core.Movies.Commands.AddMovie;
+using NSubstitute;
+using Forum.Application.Common.Mappings.Movies;
+using Forum.Application.Common.Contracts.Responses;
+using ErrorOr;
+
+
+namespace Forum.UnitTests;
+
+public class AddMovieCommandHandlerTests
+{
+    private readonly IFixture _fixture;
+    private readonly IUnitOfWork _mockUnitOfWork;
+    private readonly IMovieRepository _mockMovieRepository;
+    private readonly AddMovieCommandHandler _sut; // system under test
+    private readonly AddMovieCommandFixture _addMovieCommandFixture;
+    public AddMovieCommandHandlerTests()
+    {
+        _fixture = new Fixture().Customize(new AutoNSubstituteCustomization());
+        _mockUnitOfWork = Substitute.For<IUnitOfWork>();
+        _mockMovieRepository = Substitute.For<IMovieRepository>();
+        _mockUnitOfWork.GetRepository<IMovieRepository>().Returns(_mockMovieRepository);
+        _sut = new AddMovieCommandHandler(_mockUnitOfWork);
+        _addMovieCommandFixture = new();
+    }
+    [Fact]
+    public async Task Handle_WhenCalledWithValidCommand_ShouldReturnSuccessResult()
+    {
+        // Arrange
+        AddMovieCommand command = _addMovieCommandFixture.Create();
+        MovieEntity repositoryEntity = command.ToRepositoryEntity();
+        MovieResponse expected = repositoryEntity.ToResponse();
+        _mockMovieRepository.InsertAsync(Arg.Any<MovieEntity>(), Arg.Any<CancellationToken>()).Returns(Result.Created);
+        _mockMovieRepository.GetByIdAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>()).Returns(repositoryEntity);
+
+        // Act
+        ErrorOr<MovieResponse> result = await _sut.Handle(command, CancellationToken.None);
+        MovieResponse actual = result.Value;
+
+        // Assert
+        Assert.False(result.IsError);
+        Assert.IsType<MovieResponse>(result.Value);
+        Assert.NotNull(actual);
+        Assert.Equal(expected, actual);
+        await _mockMovieRepository.Received(1).InsertAsync(Arg.Any<MovieEntity>(), Arg.Any<CancellationToken>());
+        await _mockMovieRepository.Received(1).GetByIdAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>());
+        await _mockUnitOfWork.Received(1).SaveChangesAsync(Arg.Any<CancellationToken>());
+    }
+}
